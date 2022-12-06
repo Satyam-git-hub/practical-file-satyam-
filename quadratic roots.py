33ae7d529ed9abe4e71794522c6bb1f32f907d81
SatyamDubey <67541381+reality0101@users.noreply.github.com>
2022-12-06 10:52:54 +0530

    Add files via upload

---
+++ quadratic roots.py
@@ -0,0 +1,12 @@
+import cmath  
+a = float(input('Enter a: '))  
+b = float(input('Enter b: '))  
+c = float(input('Enter c: '))  
+  
+# calculate the discriminant  
+d = (b**2) - (4*a*c)  
+  
+# find two solutions  
+sol1 = (-b-cmath.sqrt(d))/(2*a)  
+sol2 = (-b+cmath.sqrt(d))/(2*a)  
+print(f'The solution are {sol1} and {sol2}')  
