e4675f5ae979159adbed2ca295a484519bef09e7
SatyamDubey <67541381+reality0101@users.noreply.github.com>
2022-12-09 18:57:46 +0530

    Update pract 11.py

--- pract 11.py
+++ pract 11.py
@@ -1,15 +1,10 @@
-def printIndex(str, s):
-    flag = False
-    for i in range(len(str)):
-        if (str[i:i + len(s)] == s):
-            print(i, end=" ")
-            flag = True
-
-    if (flag == False):
-        print("-1")
+str1 = input('Enter the string 1 :')
+str2 = input('Enter the string 2 :')
 
 
-str1 = input("Enter string 1")
-str2 = input("Enter string 2")
-printIndex(str1, str2)
+res = [i for i in range(len(str1)) if str1.startswith(str2, i)]
+ind=str(res)
+l=[ind,len(str2)]
+
+print("The start/ending indices of the substrings are : " ,l)
 
